c6f6fe4203730ce51bf07149644bb19aecd47fd8
Mert Dogar <mertdogar@gmail.com>
2017-10-15 05:47:14 +0300

    :sparkles: Communication: Initial centralized /dummy protocol implementation

--- stun/server.js
+++ stun/server.js
@@ -1,3 +1,4 @@
+const omit = require('lodash/omit');
 const Server = require('line-socket/server');
 const server = new Server({port: 3000});
 const {isString, isObject} = require('lodash');
@@ -5,6 +6,8 @@ const {isString, isObject} = require('lodash');
 server.on('connection', function(connection) {
     console.log(`Connection:  ${connection.id}`);
 
+    connection.on('sendMessage', message => onSendMessage(connection, message));
+    connection.on('getPeers', message => onGetPeers(connection, message));
     connection.on('publish', message => onPublish(connection, message));
     connection.on('subscribe', message => onSubscribe(connection, message));
     connection.on('unsubscribe', message => onUnsubscribe(connection, message));
@@ -47,6 +50,36 @@ function onUnsubscribe(connection, message) {
     const roomName = message.payload;
     connection.leaveRoom(getRoomName(roomName));
     message.resolve();
+}
+
+
+function onGetPeers(connection, message) {
+    const roomNames = connection.getRooms();
+    const room = server.getRoom(roomNames[0]);
+    const peerIds = Object.keys(room.getConnections())
+                        .filter(i => i != connection.id);
+    message.resolve(peerIds);
+}
+
+function onSendMessage(connection, message) {
+    if (!isObject(message.payload)) {
+        console.log('Error: Could not parse payload of send message event.');
+        return message.reject(new Error('Could not parse payload of publish event.'));
+    }
+
+    const {peerId, data} = message.payload;
+    const roomNames = connection.getRooms();
+    const room = server.getRoom(roomNames[0]);
+
+    const otherConnection = room.getConnectionById(peerId);
+
+    if (!otherConnection)
+        return message.reject(new Error(`Peer ${peerId} is offline`));
+
+    otherConnection
+        .send('peerMessage', {peerId: connection.id, data})
+        .then(response => message.resolve(response))
+        .catch(err => message.reject(err));
 }
 
 function onPing(connection, message) {
